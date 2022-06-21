f72b02686b04692fd26fc760018d7797d0942864
Ilya Yudin <nibezo.cs@gmail.com>
2022-06-21 21:42:14 +0300

    feat: add mongodb

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+    .connect('mongodb+srv://nibezo:Yyr5kUMh!Kn7J9E@cluster0.ociy4ls.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('DB is OK'))
+    .catch((err) => console.log('DB error: ', err))
 
 const app = express();
-
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello, world!');
+})
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+
+    const token = jwt.sign({
+            email: req.body.email,
+            fullName: 'Вася Пупкин',
+        }, 
+    'secret123',
+    );
+
+    res.json({
+        success: true,
+        token,
+    })
 })
 
 app.listen(4444, (err) => {
